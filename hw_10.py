b088fb7ca578f63089c0a2d0771f46ae92d04463
Liubov Iarova <lubany9999@gmail.com>
2022-06-07 00:16:01 +0200

    Testing git

--- hw_10.py
+++
@@ -1,6 +0,0 @@
-
-def test_human_grow_age(create_custom_human):
-    human_1 = create_custom_human(name='Anna', age=75, gender='male')
-    human_2 = create_custom_human(name='Petro', age=105, gender='female')
-
-
