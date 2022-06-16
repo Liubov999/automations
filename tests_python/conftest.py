969cab5dc0166c78b11c6f396b63b6005b06b414
Liubov Iarova <lubany9999@gmail.com>
2022-06-16 18:58:57 +0200

    HW 14

--- tests_python/conftest.py
+++ tests_python/conftest.py
@@ -1,56 +1,34 @@
-import logging
+import os
+
 import pytest
 
-from tests_python.code_for_testing import Human
-
-logger = logging.getLogger()
-logger.setLevel('INFO')
-
-
-@pytest.fixture()
-def create_new_human():
-
-    def human_factory(name, age, gender):
-        return Human(name=name, age=age, gender=gender)
-        yield human_factory
-    logger.info(msg='Fixture finished')
+from selenium import webdriver
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+os.environ['GH_TOKEN'] = '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'
 
 
 @pytest.fixture()
-def human_growing():
-    logger.info(msg='Fixture start')
-    yield Human(name='Ali', age=10, gender='female')
-    logger.info(msg='Fixture finished')
+def create_chrome_browser():
+    driver_chrome = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver_chrome.maximize_window()
+    yield driver_chrome
+    driver_chrome.quit()
 
 
 @pytest.fixture()
-def dead_man():
-    logger.info(msg='Fixture start')
-    yield Human(name='Ali', age=190, gender='male')
-    logger.info(msg='Fixture finished')
+def create_firefox_browser():
+    driver_firefox = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+    driver_firefox.maximize_window()
+    yield driver_firefox
+    driver_firefox.quit()
 
 
 @pytest.fixture()
-def raising():
-    logger.info(msg='Fixture start')
-    yield Human
-    logger.info(msg='Fixture finished')
-
-
-@pytest.fixture()
-def create_new_gender():
-    logger.info(msg='Fixture start')
-    yield Human
-    logger.info(msg='Fixture finished')
-
-
-@pytest.fixture()
-def create_new_friends():
-
-    logger.info(msg='Fixture start')
-
-    def new_friend(name, age, gender):
-        return Human(name=name, age=age, gender=gender)
-        yield new_friend
-    logger.info(msg='Fixture finished')
-
+def create_edge_browser():
+    driver_edge = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
+    driver_edge.maximize_window()
+    yield driver_edge
+    driver_edge.quit()
